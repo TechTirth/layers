75d8aa41e108aa02790455d8c441eb62061bb20f
TechTirth <t1eerth.v@gmail.com>
2025-06-09 16:12:54 +0530

    Refactor Navbar component layout and styles; enhance responsiveness and add framer-motion import

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
 import Button from "@/components/Button";
+import { a } from "framer-motion/client";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -11,17 +12,26 @@ const navLinks = [
 
 export default function Navbar() {
     return (
-        <section className="py-4">
-            <div className="container">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
                     <div>
                         <Image
                             src={logoImage}
                             alt={"layers logo"}
-                            className="h-9 w-auto"
+                            className="h-9 md:h-auto w-auto"
                         />
                     </div>
-                    <div className="flex justify-end">
+                    <div className="lg:flex justtify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map((link) => (
+                                <a href={link.href} key={link.label}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -38,8 +48,8 @@ export default function Navbar() {
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
-                        <Button variant="secondary">Log In</Button>
-                        <Button variant="primary">Sign Up</Button>
+                        <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
+                        <Button variant="primary" className="hidden md:inline-flex items-center">Sign Up</Button>
                     </div>
                 </div>
             </div>
